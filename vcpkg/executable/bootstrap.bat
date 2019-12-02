c928fae1ef2ff51ad1e2dcc68f860b86197fe68a
acdemiralp <demiralpali@gmail.com>
2019-12-02 12:55:09 +0100

    Added vcpkg support.

---
+++ vcpkg/executable/bootstrap.bat
@@ -0,0 +1,17 @@
+
+
+if not exist "build" mkdir build
+cd build
+if not exist "vcpkg" git clone https://github.com/Microsoft/vcpkg.git
+cd vcpkg
+if not exist "vcpkg.exe" call bootstrap-vcpkg.bat
+
+set VCPKG_COMMAND=vcpkg install --recurse
+set VCPKG_DEFAULT_TRIPLET=x64-windows
+rem Add your library ports here.
+%VCPKG_COMMAND% catch2 
+cd ..
+
+cmake -Ax64 -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake ..
+cmake --build . --target ALL_BUILD --config Release
+cd ..
