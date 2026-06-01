1a9c8e4266c93a3a09af573054957dc6bc115bf3
acdemiralp <demiralpali@gmail.com>
2026-06-02 02:51:59 +0300

    Modernization progress.

--- executable_with_vcpkg/bootstrap.bat
+++ executable_with_vcpkg/bootstrap.bat
@@ -2,16 +2,16 @@
 
 setlocal
 
-set "SCRIPT_DIR=%~dp0"
+for %%I in ("%~dp0\.") do set "SCRIPT_DIR=%%~fI"
 
-if not exist "%SCRIPT_DIR%build"                 mkdir "%SCRIPT_DIR%build"
-if not exist "%SCRIPT_DIR%build\vcpkg"           git clone https://github.com/Microsoft/vcpkg.git "%SCRIPT_DIR%build\vcpkg"
-if not exist "%SCRIPT_DIR%build\vcpkg\vcpkg.exe" call "%SCRIPT_DIR%build\vcpkg\bootstrap-vcpkg.bat"
+if not exist "%SCRIPT_DIR%\build"                 mkdir "%SCRIPT_DIR%\build"
+if not exist "%SCRIPT_DIR%\build\vcpkg"           git clone --depth 1 https://github.com/Microsoft/vcpkg.git "%SCRIPT_DIR%\build\vcpkg"
+if not exist "%SCRIPT_DIR%\build\vcpkg\vcpkg.exe" call "%SCRIPT_DIR%\build\vcpkg\bootstrap-vcpkg.bat"
 
-"%SCRIPT_DIR%build\vcpkg\vcpkg.exe" install --x-manifest-root="%SCRIPT_DIR%" --x-install-root="%SCRIPT_DIR%build\vcpkg_installed"
+"%SCRIPT_DIR%\build\vcpkg\vcpkg.exe" install --x-manifest-root="%SCRIPT_DIR%" --x-install-root="%SCRIPT_DIR%\build\vcpkg_installed"
 
-for /f "usebackq delims=" %%i in (`"%SCRIPT_DIR%build\vcpkg\vcpkg.exe" fetch cmake`) do set "CMAKE=%%i"
+for /f "usebackq delims=" %%i in (`"%SCRIPT_DIR%\build\vcpkg\vcpkg.exe" fetch cmake`) do set "CMAKE=%%i"
 "%CMAKE%" -S "%SCRIPT_DIR%" --preset ninja-multi
-"%CMAKE%" --build "%SCRIPT_DIR%build\ninja-multi" --preset release --parallel
+"%CMAKE%" --build "%SCRIPT_DIR%\build\ninja-multi" --preset release --parallel
 
 endlocal
--- header_only_library_with_vcpkg/bootstrap.sh
+++ header_only_library_with_vcpkg/bootstrap.sh
@@ -4,9 +4,9 @@
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
-if [ ! -d "$SCRIPT_DIR/build"             ]; then mkdir "$SCRIPT_DIR/build"                                                 ; fi
-if [ ! -d "$SCRIPT_DIR/build/vcpkg"       ]; then git clone https://github.com/Microsoft/vcpkg.git "$SCRIPT_DIR/build/vcpkg"; fi
-if [ ! -x "$SCRIPT_DIR/build/vcpkg/vcpkg" ]; then "$SCRIPT_DIR/build/vcpkg/bootstrap-vcpkg.sh"                              ; fi
+if [ ! -d "$SCRIPT_DIR/build"             ]; then mkdir "$SCRIPT_DIR/build"                                                           ; fi
+if [ ! -d "$SCRIPT_DIR/build/vcpkg"       ]; then git clone --depth 1 https://github.com/Microsoft/vcpkg.git "$SCRIPT_DIR/build/vcpkg"; fi
+if [ ! -x "$SCRIPT_DIR/build/vcpkg/vcpkg" ]; then "$SCRIPT_DIR/build/vcpkg/bootstrap-vcpkg.sh"                                        ; fi
 
 "$SCRIPT_DIR/build/vcpkg/vcpkg" install --x-manifest-root="$SCRIPT_DIR" --x-install-root="$SCRIPT_DIR/build/vcpkg_installed"
 
